9aa21a4a5685d2da5dc35c8d18a516691bd2ae36
Gauravsawant34 <gauravsawant3198@gmail.com>
2024-03-04 23:30:31 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,40 @@
+// $(document).ready(function() {
+  
+//     $('.navbar-collapse ul li a').click(function(){
+//      /* always close responsive nav after click */
+//     $('.navbar-toggle:visible').click();
+//       }); 
+    
+//    $('a[href*="#"]:not([href="#"])').click((e) => {
+//       const target = $(e.target.hash);
+  
+//       if (target.length) {
+//         $('html, body').animate({
+//           scrollTop: target.offset().top - 50
+//         }, 1000);
+  
+//         return false;
+//       }
+//     });
+//   });
+
+
+$(document).ready(function() {
+  
+    $('.navbar-collapse ul li a').click(function(){
+     /* always close responsive nav after click */
+    $('.navbar-toggle:visible').click();
+      }); 
+    
+   $('a[href*="#"]:not([href="#"])').click((e) => {
+      const target = $(e.target.hash);
+  
+      if (target.length) {
+        $('html, body').animate({
+          scrollTop: target.offset().top - 50
+        }, 1000);
+  
+        return false;
+      }
+    });
+  });
